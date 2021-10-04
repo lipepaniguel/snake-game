3681ee2f932e745a4db4e56f366862395d1e8404
lipepaniguel <lipe.paniguel@gmail.com>
2021-10-04 14:57:42 -0300

    limpeza e correção no 'main'

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from apple import maca, nova_apple
 from snake import celeste
 import random
 
-continuar = True
-
 
 def comecar():
     global continuar
@@ -17,28 +15,25 @@ canvas.setup(400, 400)
 canvas.screensize(370, 370)
 canvas.bgcolor('black')
 
-nova_apple()
-
 canvas.listen()
 canvas.onkeypress(celeste.sobe, 'w')
 canvas.onkeypress(celeste.desce, 's')
 canvas.onkeypress(celeste.direita, 'd')
 canvas.onkeypress(celeste.esquerda, 'a')
-# canvas.onkeypress(celeste.movimento, 'space')
+
+nova_apple()
 
 comprimento = 0
 score = 0
 
-tamanho_tail = [0]
-tail = [0]
-ultimo_rabo = []
+tamanho_tail = [0, 1]
+tail = [0, 1, 2]
 
+continuar = True
 
 while continuar:
-    # print(len(tail))
     print(tail)
     celeste.movimento()
-    ultimo_rabo = celeste.pos()
 
     j = -1
 
@@ -46,20 +41,21 @@ while continuar:
         tail[j] = tail[j - 1]
         j -= 1
 
-    tail[0] = ultimo_rabo
+    tail[0] = (round(celeste.xcor()), round(celeste.ycor()))
 
-    if celeste.pos() in tail[1:]:
-        continuar = False
+    if (round(celeste.xcor()), round(celeste.ycor())) in tail[1:]:
         break
 
-    if celeste.xcor() >= 190 or celeste.xcor() <= -190:
+    if round(celeste.xcor()) >= 190 or round(celeste.xcor()) <= -190:
         break
-    if celeste.ycor() >= 190 or celeste.ycor() <= -190:
+    if round(celeste.ycor()) >= 190 or round(celeste.ycor()) <= -190:
         break
+
     if celeste.distance(maca) < 7:
         comprimento += 2
         score += 1
-        tail.append(score)
+        tail.append(tamanho_tail[0])
+        tail.append(tamanho_tail[1])
 
         nova_apple()
 
