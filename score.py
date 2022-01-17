68c2fad5e67c97994ed4758ae4da319fb5ddc9ab
lipepaniguel <lipe.paniguel@gmail.com>
2022-01-17 14:20:43 -0300

    implementação de record

--- score.py
+++ score.py
@@ -10,7 +10,9 @@ class Placar(Turtle):
         self.speed(0)
         self.color('white')
         self.score = 0
-        self.new_score = 0
+        with open("records.txt", mode="r") as registro_record:
+            conteudo_records = int(float(registro_record.read()))
+        self.new_score = conteudo_records
 
     def score_up(self):
         self.score += 1
@@ -22,11 +24,15 @@ class Placar(Turtle):
         self.write(
             f'Score: {self.score}', align='center', font=('Terminal', 14)
             )
+
         if self.score > self.new_score:
             self.new_score = self.score
+            with open("records.txt", mode="w") as registro:
+                registro.write(f'{self.new_score}')
             self.goto(0, -40)
             self.write(
                 f'New Record: {self.new_score} !',
                 align='center', font=('Terminal', 14)
                 )
-            self.score = 0
+
+        self.score = 0
